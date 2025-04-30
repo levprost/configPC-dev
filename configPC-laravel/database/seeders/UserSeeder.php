2864d087584706747243a0954fbd3a655ac2a664
levprost <smolev17@gmail.com>
2025-04-30 14:56:39 +0200

    +userSeed

---
+++ configPC-laravel/database/seeders/UserSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'nick_name' => 'Administrateur',
+            'role' => 'ROLE_ADMIN',
+            'email' => 'admin@truc.fr',
+            'email_verified_at' => now(),
+            'password' => Hash::make('test123'),
+            'remember_token' => Str::random(10)
+        ]);
+        User::factory()->create([
+            'nick_name' => 'Editeur',
+            'role' => 'ROLE_EDITOR',
+            'email' => 'edit@truc.fr',
+            'email_verified_at' => now(),
+            'password' => Hash::make('test123'),
+            'remember_token' => Str::random(10)
+        ]);
+        User::factory()->create([
+            'nick_name' => 'Modérateur',
+            'role' => 'ROLE_MODERATOR',
+            'email' => 'modo@truc.fr',
+            'email_verified_at' => now(),
+            'password' => Hash::make('test123'),
+            'remember_token' => Str::random(10)
+        ]);
+        User::create([
+            'nick_name' => 'Utilisateur',
+            'role' => 'ROLE_USER',
+            'email' => 'user@truc.fr',
+            'password' => Hash::make('test123'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10)
+        ]);
+        User::factory(10)->create();
+    }
+}
