4de5390bed41a44757cb461b579e247f36cc60e8
levprost <smolev17@gmail.com>
2025-05-02 00:41:14 +0200

    try

--- configPC-laravel/app/Policies/PostPolicy.php
+++ configPC-laravel/app/Policies/PostPolicy.php
@@ -10,14 +10,14 @@ class PostPolicy
 {
     public function update(User $user, Post $post)
     {
-        return $user->id === $post->user_id || $user->role === 'ROLE_MODO' || $user->role === 'ROLE_ADMIN'
+        return $user->id === $post->user_id || $user->role === 'ROLE_MODERATOR' || $user->role === 'ROLE_ADMIN'
             ? Response::allow()
             : Response::deny('You do not own this post.');
     }
 
     public function delete(User $user, Post $post)
     {
-        return $user->id === $post->user_id || $user->role === 'ROLE_EDITOR' || $user->role === 'ROLE_ADMIN'
+        return $user->id === $post->user_id || $user->role === 'ROLE_MODERATOR' || $user->role === 'ROLE_ADMIN'
             ? Response::allow()
             : Response::deny('You do not own this post.');
     }
